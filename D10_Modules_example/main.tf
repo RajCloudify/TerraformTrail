663c07dc18f29e1d10f1ebd1ff1d88c38acaa101
RajCloudify <rajattingal1@gmail.com>
2026-06-16 06:59:08 +0530

    D10 Added

---
+++ D10_Modules_example/main.tf
@@ -0,0 +1,13 @@
+module "s3_bucket" {
+    source = "github.com/RajCloudify/terraform-aws-s3-bucket.git"
+
+    buckett = "rajcloudify-terraform-s3-bucket"
+    acl    = "private"
+
+    control_object_ownership = true
+    object_ownership         = "BucketOwnerPreferred"
+
+    versioning = {
+        enabled = true
+    }
+}
--- D9.1.2_Values_passing_from_modules_9.0.1/main.tf
+++ D9.1.2_Values_passing_from_modules_9.0.1/main.tf
@@ -6,3 +6,4 @@ module "test" {
   tags = var.tags
 
 }
+
