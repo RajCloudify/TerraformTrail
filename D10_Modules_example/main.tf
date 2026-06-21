f54e3f905b308c4dd1121bab0120bf68c4d05f28
Raj Attingal <rajattingal1@gmail.com>
2026-06-21 19:25:39 +0530

    Fix formatting of source line in main.tf

--- D10_Modules_example/main.tf
+++ D10_Modules_example/main.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-    source = "github.com/RajCloudify/terraform-aws-s3-bucket.git"
+    source = "github.com/RajCloudify/terraform-aws-s3-bucket.git" 
 
     buckett = "rajcloudify-terraform-s3-bucket"
     acl    = "private"
